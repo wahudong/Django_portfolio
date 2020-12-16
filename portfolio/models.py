fd2bc77c1c81d00df9c910bfe19b00c936a5aa95
wahudong <wahudong@gmail.com>
2020-12-16 12:25:27 -0600

    after first migration

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+  """
+  Project class
+  """
+  title = models.CharField(max_length=100)
+  description = models.CharField(max_length=250)
+  image = models.ImageField(upload_to='portfolio/images/')
+  url = models.URLField(blank=True)
