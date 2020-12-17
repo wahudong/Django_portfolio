1fe4ee789fda28c0d71fffd90bcf5782df8c0540
wahudong <wahudong@gmail.com>
2020-12-17 17:39:04 -0600

    added blog detail

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Blog
 
 # Create your views here.
 def all_blogs(request):
-  blogs = Blog.objects.order_by('-date')[:2]
+  # blogs = Blog.objects.order_by('-date')[0:2]
+  blogs = Blog.objects.order_by('-date')
   return render(request,'blog/all_blogs.html',{'blogs':blogs})
+
+def detail(request,blog_id):
+  """
+  detail of blog
+  """
+  blog = get_object_or_404(Blog, pk=blog_id)
+  return render(request, 'blog/detail.html', {'blog':blog})
+
